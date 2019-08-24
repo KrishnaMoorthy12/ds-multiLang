ca7e5461b93a7efcc5b58b8852db75fe10e3fa4d
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-08-24 20:47:46 +0530

    added 2 more functions 1. insert at middle 2. remove an element

--- sll.c
+++ sll.c
@@ -23,12 +23,31 @@ void append(list *list1){
   printf("Element added succesfully.");
 }
 
+void insert(list *list1){
+  int key;
+  printf("\nWhere do you want to insert? Before ");
+  scanf("%d", &key);
+  list *tmp = list1;
+  while (tmp->next->next!=NULL && key != tmp->next->data){
+    tmp = tmp ->next;
+  }
+  if (tmp -> next ->data == key){
+    list *tmp1 = createNode();
+    tmp1 -> next = tmp ->next;
+    tmp -> next = tmp1;
+  }
+  else
+    printf("\nElement not found.");
+}
+
 list* newList() {
   int n;
   list *newList = (list*)malloc(sizeof(list));
   printf("\nHow many elements do you want to add?");
   scanf("%d", &n);
-  for (int i = 0; i < n; i++) {
+  newList = createNode();
+  printf("Element added succesfully.");
+  for (int i = 0; i < n-1; i++) {
     append(newList);
   }
   printf("\n\nList created succesfully.");
@@ -41,6 +60,22 @@ void pop(list *list1){
   printf("Popped element: %d", tmp->next->data);
   free(tmp->next);
   tmp->next = NULL;
+}
+
+void delete(list *list1){
+  int key;
+  printf("\nWhat do you want to remove? ");
+  scanf("%d", &key);
+  list *tmp = list1;
+  while (tmp->next!=NULL && key != tmp->data){
+    tmp = tmp ->next;
+  }
+  //needs memory optimization
+  if (tmp -> data == key){
+    tmp -> next = tmp -> next ->next;
+  }
+  else
+    printf("\nElement not found.");
 }
 
 void display(list *list1){
@@ -64,9 +99,11 @@ int main(int argc, char const *argv[]) {
   while (1) {
     printf("\n==== MENU ====");
     printf("\n1. Create");
-    printf("\n2. Insert");
-    printf("\n3. Pop");
-    printf("\n4. Display");
+    printf("\n2. Append");
+    printf("\n3. Insert");
+    printf("\n4. Pop");
+    printf("\n5. Delete");
+    printf("\n6. Display");
     printf("\n0. Exit");
     printf("\n\nEnter your choice: ");
     scanf("%d", &ch);
@@ -76,16 +113,23 @@ int main(int argc, char const *argv[]) {
         list *list1 = newList();
         break;
       case 2:
-        printf("\nInsert a new node...");
+        printf("\nAppend a new node...");
         append(list1);
         break;
       case 3:
-        pop(list1);
+        printf("\nInset a new node...");
+        insert(list1);
         break;
       case 4:
-        display(list1);
+        pop(list1);
         break;
       case 5:
+        delete(list1);
+        break;
+      case 6:
+        display(list1);
+        break;
+      case 7:
       case 0:
         return 0;
       default:
