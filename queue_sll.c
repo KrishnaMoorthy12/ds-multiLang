0688c93c241ef7f33e094c72fda43e5b6d5343d5
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-10-11 16:49:51 +0530

    nothing new

--- queue_sll.c
+++ queue_sll.c
@@ -1,43 +1,50 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-struct node{
+struct node
+{
   int data;
   struct node *next;
 };
 
 typedef struct node list;
 
-list* createNode() {
-  list *newNode = (list*)malloc(sizeof(list));
+list *createNode()
+{
+  list *newNode = (list *)malloc(sizeof(list));
   printf("\nEnter the value: ");
   scanf("%d", &newNode->data);
   newNode->next = NULL;
   return newNode;
 }
 
-void enqueue(list *list1){
+void enqueue(list *list1)
+{
   list *tmp = list1;
-  for (; tmp->next != NULL; tmp = tmp->next);
-  tmp -> next = createNode();
+  for (; tmp->next != NULL; tmp = tmp->next)
+    ;
+  tmp->next = createNode();
   printf("Element added the queue.");
 }
 
-list* newList() {
-  int n;
-  list *newList = (list*)malloc(sizeof(list));
+list *newList()
+{
+  int n, i;
+  list *newList = (list *)malloc(sizeof(list));
   printf("\nHow many elements do you want to add? ");
   scanf("%d", &n);
   newList = createNode();
   printf("Element added succesfully.");
-  for (int i = 0; i < n-1; i++)
+  for (i = 0; i < n - 1; i++)
     enqueue(newList);
   printf("\n\nList created succesfully.");
   return newList;
 }
 
-void dequeue(list *list1){
-  if (list1 == NULL){
+void dequeue(list *list1)
+{
+  if (list1 == NULL)
+  {
     printf("\nThe queue is empty.");
     return;
   }
@@ -47,24 +54,28 @@ void dequeue(list *list1){
   free(tmp);
 }
 
-void display(list *list1){
+void display(list *list1)
+{
   list *temp = list1;
   printf("\nThe elements are: ");
-  while(temp!=NULL){
+  while (temp != NULL)
+  {
     printf("%d", temp->data);
     if (temp->next != NULL)
       printf(", ");
     else
       printf(".");
-    temp=temp->next;
+    temp = temp->next;
   }
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
   list *list1;
-  list1 = (list*)malloc(sizeof(list));
+  list1 = (list *)malloc(sizeof(list));
   int ch;
-  while (1) {
+  while (1)
+  {
     printf("\n\n==== MENU ====");
     printf("\n1. Create");
     printf("\n2. Enqueue");
@@ -73,26 +84,27 @@ int main(int argc, char const *argv[]) {
     printf("\n0. Exit");
     printf("\n\nEnter your choice: ");
     scanf("%d", &ch);
-    switch (ch) {
-      case 1:
-        printf("\nCreate new Queue...");
-        list *list1 = newList();
-        break;
-      case 2:
-        enqueue(list1);
-        break;
-      case 3:
-        dequeue(list1);
-        break;
-      case 4:
-        display(list1);
-        break;
-      case 5:
-      case 0:
-        printf("\nExiting...\n");
-        return 0;
-      default:
-        printf("Invalid input.");
+    switch (ch)
+    {
+    case 1:
+      printf("\nCreate new Queue...");
+      list *list1 = newList();
+      break;
+    case 2:
+      enqueue(list1);
+      break;
+    case 3:
+      dequeue(list1);
+      break;
+    case 4:
+      display(list1);
+      break;
+    case 5:
+    case 0:
+      printf("\nExiting...\n");
+      return 0;
+    default:
+      printf("Invalid input.");
     }
   }
   return 0;
--- sll.c
+++ sll.c
@@ -1,98 +1,112 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-struct node{
+struct node
+{
   int data;
   struct node *next;
 };
 
 typedef struct node list;
 
-list* createNode() {
-  list *newNode = (list*)malloc(sizeof(list));
+list *createNode()
+{
+  list *newNode = (list *)malloc(sizeof(list));
   printf("\nEnter the value: ");
   scanf("%d", &newNode->data);
   newNode->next = NULL;
   return newNode;
 }
 
-void append(list *list1){
+void append(list *list1)
+{
   list *tmp = list1;
-  for (; tmp->next != NULL; tmp = tmp->next);
-  tmp -> next = createNode();
+  for (; tmp->next != NULL; tmp = tmp->next)
+    ;
+  tmp->next = createNode();
   printf("Element added succesfully.");
 }
 
-void insert(list *list1){
+void insert(list *list1)
+{
   int key;
   printf("\nWhere do you want to insert? Before ");
   scanf("%d", &key);
   list *tmp = list1;
-  while (tmp->next->next!=NULL && key != tmp->next->data)
-    tmp = tmp ->next;
-  if (tmp -> next ->data == key){
+  while (tmp->next->next != NULL && key != tmp->next->data)
+    tmp = tmp->next;
+  if (tmp->next->data == key)
+  {
     list *tmp1 = createNode();
-    tmp1 -> next = tmp ->next;
-    tmp -> next = tmp1;
+    tmp1->next = tmp->next;
+    tmp->next = tmp1;
   }
   else
     printf("\nElement not found.");
 }
 
-list* newList() {
+list *newList()
+{
   int n;
-  list *newList = (list*)malloc(sizeof(list));
+  list *newList = (list *)malloc(sizeof(list));
   printf("\nHow many elements do you want to add? ");
   scanf("%d", &n);
   newList = createNode();
   printf("Element added succesfully.");
-  for (int i = 0; i < n-1; i++)
+  for (int i = 0; i < n - 1; i++)
     append(newList);
   printf("\n\nList created succesfully.");
   return newList;
 }
 
-void pop(list *list1){
+void pop(list *list1)
+{
   list *tmp = list1;
-  for(;tmp->next->next != NULL; tmp = tmp->next);
+  for (; tmp->next->next != NULL; tmp = tmp->next)
+    ;
   printf("Popped element: %d", tmp->next->data);
   free(tmp->next);
   tmp->next = NULL;
 }
 
-void delete(list *list1){
+void delete (list *list1)
+{
   int key;
   printf("\nWhat do you want to remove? ");
   scanf("%d", &key);
   list *tmp = list1;
-  while (tmp->next!=NULL && key != tmp->data)
-    tmp = tmp ->next;
+  while (tmp->next != NULL && key != tmp->data)
+    tmp = tmp->next;
 
   //needs memory optimization
-  if (tmp -> data == key)
-    tmp -> next = tmp -> next ->next;
+  if (tmp->data == key)
+    tmp->next = tmp->next->next;
   else
     printf("\nElement not found.");
 }
 
-void display(list *list1){
+void display(list *list1)
+{
   list *temp = list1;
   printf("\nThe elements are: ");
-  while(temp!=NULL){
+  while (temp != NULL)
+  {
     printf("%d", temp->data);
     if (temp->next != NULL)
       printf(", ");
     else
       printf(".");
-    temp=temp->next;
+    temp = temp->next;
   }
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
   list *list1;
-  list1 = (list*)malloc(sizeof(list));
+  list1 = (list *)malloc(sizeof(list));
   int ch;
-  while (1) {
+  while (1)
+  {
     printf("\n==== MENU ====");
     printf("\n1. Create");
     printf("\n2. Append");
@@ -103,34 +117,35 @@ int main(int argc, char const *argv[]) {
     printf("\n0. Exit");
     printf("\n\nEnter your choice: ");
     scanf("%d", &ch);
-    switch (ch) {
-      case 1:
-        printf("\nCreate new Singly Linked List...");
-        list *list1 = newList();
-        break;
-      case 2:
-        printf("\nAppend a new node...");
-        append(list1);
-        break;
-      case 3:
-        printf("\nInset a new node...");
-        insert(list1);
-        break;
-      case 4:
-        pop(list1);
-        break;
-      case 5:
-        delete(list1);
-        break;
-      case 6:
-        display(list1);
-        break;
-      case 7:
-      case 0:
-        printf("\nExiting...\n");
-        return 0;
-      default:
-        printf("Invalid input.");
+    switch (ch)
+    {
+    case 1:
+      printf("\nCreate new Singly Linked List...");
+      list *list1 = newList();
+      break;
+    case 2:
+      printf("\nAppend a new node...");
+      append(list1);
+      break;
+    case 3:
+      printf("\nInset a new node...");
+      insert(list1);
+      break;
+    case 4:
+      pop(list1);
+      break;
+    case 5:
+      delete (list1);
+      break;
+    case 6:
+      display(list1);
+      break;
+    case 7:
+    case 0:
+      printf("\nExiting...\n");
+      return 0;
+    default:
+      printf("Invalid input.");
     }
   }
   return 0;
